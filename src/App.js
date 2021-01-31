6ba339f8166936da208d153079330da91f8755bd
satishl12697 <satishl12697@gmail.com>
2021-01-31 12:03:42 +0530

    wordpress project.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Testimonials from "./Components/Testimonials";
 import Portfolio from "./Components/Portfolio";
+import Portfolio1 from "./Components/Portfolio1";
 
 class App extends Component {
   constructor(props) {
@@ -45,6 +46,7 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <Portfolio data={this.state.resumeData.portfolio} />
+        <Portfolio1 data={this.state.resumeData.portfolio1} />
         {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
